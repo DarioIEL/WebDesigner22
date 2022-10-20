3d8b4234163760428f90491fc570d568ccb0fcf5
Dario <104058513+DarioIEL@users.noreply.github.com>
2022-10-20 12:52:07 +0200

    Add files via upload

---
+++ JAVASCRIPT/Lez1-INTRO/js/script06OpTernario.js
@@ -0,0 +1,15 @@
+//famiglia dei controlli if
+//<condizione>?<istruzione1>:<istruzione2>
+
+var miaCondizione = false;
+
+miaCondizione?console.log("La condizione è vera"): console.log("La condizione è falsa");
+
+
+var miaEta = 17;
+
+(miaEta < 18)?console.log("Mi dispiace non puoi accedere"): console.log("Benvenuto");
+
+var interruttore = "ON";
+
+(interruttore == "ON")?console.log("lampadina accesa"): console.log("lampadina spenta");;
