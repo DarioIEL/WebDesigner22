f70ac2d560f6dd4049e5b55686f71dd9b27a4605
Dario <104058513+DarioIEL@users.noreply.github.com>
2022-10-28 10:33:00 +0200

    Add files via upload

--- JAVASCRIPT/Lez3-Oggetti/js/script04.js
+++ JAVASCRIPT/Lez3-Oggetti/js/script04.js
@@ -9,7 +9,9 @@ function Studente(nome, cognome, matricola, anno){
         var msg = "Nome: " + this.nome + " - Cognome: " + this.cognome + " - Matricola: " + this.matricola + " - Anno Iscrizione: " + this.anno;
         return msg;
     }
+
 }
+
 
 var classe = [
     new Studente('Olga', 'Koval', 1, 2022),
@@ -32,23 +34,91 @@ var classe = [
 //prendo il pulsante
 var btn = document.getElementById('btn');
 
+function suggerisciMatricola(){
+document.getElementById('matricola').value = classe[classe.length-1].matricola + 1;
+}
+
+suggerisciMatricola();
+
 function registraStudente(){
   
-    var nome = document.getElementById('nome').value;
-    var cognome = document.getElementById('cognome').value;
-    var matricola = Number(document.getElementById('matricola').value);
-    var anno = Number(document.getElementById('anno').value);
+    if(controllaCampi()){
+        var nome = document.getElementById('nome').value;
+        var cognome = document.getElementById('cognome').value;
+        var matricola = Number(document.getElementById('matricola').value);
+        var anno = Number(document.getElementById('anno').value);
 
-    var studente = new Studente(nome, cognome, matricola, anno);
+        var studente = new Studente(nome, cognome, matricola, anno);
+        classe.push(studente);
 
-    classe.push(studente);
+        stampaLista();
+        pulisciCampiInput();
+        suggerisciMatricola();
+
+    }else{
+        alert('Hai dimenticato di compilare qualche campo')
+    }
 
     //stampa lista studenti nello HTML
-    console.log(classe);
+    console.log(classe);    
 }
+
 
 btn.onclick = registraStudente;
 
 //Stampa della lista studenti: al caricamento pagina e quando aggiungo un nuovo studente
 //Matricola autoincrementale
 //MOLTO OPZIONALE: inserisci un pulsante per l'eliminazione dello studente
+var listaStudenti = document.getElementById('listaStudenti');
+
+function stampaLista(){
+    listaStudenti.innerHTML = '';
+
+    classe.forEach(studente => {
+        listaStudenti.innerHTML += studente.presentati() + "<button onclick=eliminaStud('studente')>Elimina</button> <br>"
+    });
+}
+
+stampaLista();
+
+function pulisciCampiInput(){
+    document.getElementById('nome').value = '';
+    document.getElementById('cognome').value = '';
+    document.getElementById('matricola').value = '';
+    document.getElementById('anno').value = '';
+}
+
+function controllaCampi(){
+  
+    if(document.getElementById('nome').value == '' || document.getElementById('cognome').value == ''|| document.getElementById('matricola').value == '' || document.getElementById('anno').value == ''){
+
+        return false;
+    }else{
+        return true;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ///DA spiegare più avanti
+// Studente.prototype.faiQualcosa = function(){
+//     console.log("Ciao sto facendo qualcosa");
+// }
+
+// var mioStudente = new Studente("Gennaro", "Esposito", 42, 2022 );
+
+// mioStudente.faiQualcosa();
+
